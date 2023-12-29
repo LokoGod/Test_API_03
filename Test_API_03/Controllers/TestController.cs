e00bbfdd345d16962b39f12d8aa1b344d720304b
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-29 11:15:16 +0530

    Init

---
+++ Test_API_03/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test_API_03.Models;
+using Test_API_03.Models.DTO;
+
+namespace Test_API_03.Controllers
+{
+    //[Route("api/v1/[controller]"]
+    [Route("api/v1/Test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<TestDTO> GetTestModels()
+        {
+            return new List<TestDTO>
+            {
+                new TestDTO{Id = 1, Name = "Kratos"},
+                new TestDTO{Id = 2, Name = "Batman"}
+            };
+        }
+    }
+}
